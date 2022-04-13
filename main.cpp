be1af734f195915f54452a8103f275cd9a5fdf9b
oharakogakuin <ohara@g.neec.ac.jp>
2022-04-13 11:09:24 +0900

    変更テスト

--- main.cpp
+++ main.cpp
@@ -20,6 +20,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	win = WinApp::GetInstance();
 	win->CreateGameWindow("LE2D_99_オオハラ_ヒデフミ_AL3");
 
+	// 変更テスト
+
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
 	dxCommon->Initialize(win);
